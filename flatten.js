f9c24b3e549b49340987e1f0142cea40c54011e8
vish052 <37632240+vish052@users.noreply.github.com>
2025-06-16 12:28:04 +0530

    Update flatten.js

--- flatten.js
+++ flatten.js
@@ -1,21 +1,24 @@
-"use strict";
+/**
+ * Flattens an array up to a specified depth.
+ *
+ * This function creates a new array with all sub-array elements concatenated into it
+ * recursively up to the specified depth. It leverages the native `Array.prototype.flat()`
+ * for optimal performance.
+ *
+ * @param {Array} arr The array to flatten.
+ * @param {number} [depth=Infinity] The number of levels to flatten.
+ * Defaults to `Infinity` for deep flattening (all levels).
+ * @returns {Array} A new array with the sub-array elements concatenated.
+ * @throws {Error} If the input `arr` is not an array.
+ */
+const flattenArray = function(arr, depth = Infinity) {
+    // Robust input validation using Array.isArray()
+    if (!Array.isArray(arr)) {
+        throw new Error('Input must be an array!');
+    }
 
-var result = [];
-var flatten = function(val) {
-        if(val.constructor === Array) {
-            for(var i=0; i<val.length; i++){
-                if(val[i]){
-                    if(val[i].constructor === Array) {
-                        flatten(val[i]);
-                    } else {
-                        result.push(val[i]);
-                    }
-                }
-            }
-            return result;
-        } else {
-            throw new Error('Not an array!');
-        }
+    // Use the native Array.prototype.flat() method for efficiency and simplicity
+    return arr.flat(depth);
 };
 
-module.exports = flatten;
+module.exports = flattenArray;
